864c3579fe5ae192517042b14f189da2af999dbb
Vitor Reis <reis_vitor@outlook.com>
2024-08-31 23:48:01 -0300

    feat: add splash-screen

---
+++ controller/SplashController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class SplashController implements Initializable {
+  @FXML
+  private VBox mainBox;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // TODO Auto-generated method stub
+
+  }
+}
