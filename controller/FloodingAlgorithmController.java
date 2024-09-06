aa7a3bdfdcb7245a28abe9b1edfd3e0657879ace
Vitor Reis <reis_vitor@outlook.com>
2024-09-06 17:35:38 -0300

    feat: floodingAlgorithm controller

---
+++ controller/FloodingAlgorithmController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import model.Connection;
+import model.Router;
+
+public class FloodingAlgorithmController implements Initializable {
+
+  public void routerPositions() {
+  }
+
+  public void addRouter(Router router, double x, double y) {
+  }
+
+  public void addConnection(Connection connection) {
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // TODO Auto-generated method stub
+  }
+
+}
